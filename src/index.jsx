2f9587bbb0c00cad3a53bfbcc9b630246472ccf4
LL <liaposhchenkol@gmail.com>
2024-01-09 22:18:37 +0100

    Add ladubugState

--- src/index.jsx
+++ src/index.jsx
@@ -6,38 +6,46 @@ import './style.css';
 const STEP_SIZE = 25;
 
 const App = () => {
-  const [posX, setPosX] = useState(100);
-  const [posY, setPosY] = useState(100);
-  const [orientation, setOrientation] = useState('right');
-  
+  const [ladybugState, setLedybugState] = useState({
+    posX: 100,
+    posY: 100,
+    orientation: 'right',
+  });
+
   const handleKeyUp = ({ code }) => {
     if (code === 'ArrowUp') {
-      setOrientation('up');
-      setPosX(posX - STEP_SIZE);
+      setLedybugState({
+        ...ladybugState,
+        orientation: 'up',
+        posX: ladybugState.posX - STEP_SIZE,
+      });
     } else if (code === 'ArrowLeft') {
-      setOrientation('left');
-      setPosY(posY - STEP_SIZE);
+      setLedybugState({
+        ...ladybugState,
+        orientation: 'left',
+        posY: ladybugState.posY - STEP_SIZE,
+      });
     } else if (code === 'ArrowRight') {
-      setOrientation('right');
-      setPosY(posY + STEP_SIZE);
+      setLedybugState({
+        ...ladybugState,
+        orientation: 'right',
+        posY: ladybugState.posY + STEP_SIZE,
+      });
     } else if (code === 'ArrowDown') {
-      setOrientation('down');
-      setPosX(posX + STEP_SIZE);
+      setLedybugState({
+        ...ladybugState,
+        orientation: 'down',
+        posX: ladybugState.posX + STEP_SIZE,
+      });
     }
   };
 
   return (
-    <div 
-      tabIndex={-1}
-      className="field"
-      onKeyDown={handleKeyUp}
-    >
+    <div tabIndex={-1} className="field" onKeyDown={handleKeyUp}>
       <header>Click anywhere to start the game</header>
-      <Ladybug posX={posX} posY={posY} orientation={orientation}/>
+      <Ladybug ladybugState={ladybugState} />
     </div>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
